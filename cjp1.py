86789246ce0c0f1927c926c7ac34079d99cef712
ysschnell <schnellyas@gmail.com>
2025-10-02 12:17:30 -0700

    Coding Journal Prompt #1 Solution

--- cjp1.py
+++ cjp1.py
@@ -1,8 +1,6 @@
-#This program wil write Hello World!
+#Coding Journal Prompt #1: Writing my name, pronouns, favorite movie and favorite food!
 
-
-#This function tells the computer to print "Hello World"
-def PrintHelloWorld():
+def PrintPrompt1():
 	print(f"Full name: Yasmin Schnell")
 	print(f"Pronouns (optional): She/her")
 	print(f"Favorite movie: Bones and All")
@@ -10,7 +8,7 @@ def PrintHelloWorld():
 
 #This defines our main() function for our program
 def main():
-	PrintHelloWorld()
+	PrintPrompt1()
 
 #When we run the program, this executes first: Python has a failsafe to allow us to write complicated programs
 if __name__=="__main__":
