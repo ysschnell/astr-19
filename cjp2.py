d257eb37457fabb6fa66d8b6754a6d1aaab4ea13
ysschnell <schnellyas@gmail.com>
2025-10-02 17:56:41 -0700

    Code Journal Prompt #2 Solution

---
+++ cjp2.py
@@ -0,0 +1,30 @@
+#Code Journal Prompt #2
+import numpy as np
+
+#Sum of two floating point numbers
+def main():
+    x= 20.0
+    y=15.0
+    print("x+y =", x+y)
+    print(type(x+y))  
+if __name__ == "__main__":
+    main()
+
+#Difference between two integers
+def main():
+    x = 2
+    y = 3
+    print("x-y =", x-y)
+    print(type(x-y))
+if __name__ == "__main__":
+    main()
+
+#The product of a floating point number and an integer
+def main():
+    x = 1.0
+    y = 5
+    print("x*y",x*y)
+    print(type(x*y))
+
+if __name__ == "__main__":
+    main()
